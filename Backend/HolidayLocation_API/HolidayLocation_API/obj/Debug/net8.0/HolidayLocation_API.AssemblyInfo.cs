130fd50ce3cb99114e9457152fcc8438fe8d61ac
Darius Ionica <dariusionica2000@gmail.com>
2025-11-10 18:08:08 +0200

    Added Delete and Edit endpoints for users

--- Backend/HolidayLocation_API/HolidayLocation_API/obj/Debug/net8.0/HolidayLocation_API.AssemblyInfo.cs
+++ Backend/HolidayLocation_API/HolidayLocation_API/obj/Debug/net8.0/HolidayLocation_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolidayLocation_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064818dab5af12b3279ad46d1a74c63f1351b9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6495978bd38e1e8eb011bf6ad56e3b7664afccdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolidayLocation_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolidayLocation_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
